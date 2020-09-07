9ebac6de5ef0032807bd736bcf26ba1c7a103c04
c332030 <c332030@gmail.com>
2020-09-07 19:46:08 +0800

    feat: 添加 设置报文头方法

--- c-web-servlet/c-web-servlet-util/src/main/java/com/c332030/web/servlet/util/ServletUtils.java
+++ c-web-servlet/c-web-servlet-util/src/main/java/com/c332030/web/servlet/util/ServletUtils.java
@@ -2,6 +2,7 @@ package com.c332030.web.servlet.util;
 
 import java.net.URLConnection;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nonnull;
 
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.common.collect.ImmutableList;
 
 import com.c332030.util.collection.ArrayUtils;
+import com.c332030.util.data.StringUtils;
 
 /**
  * <p>
@@ -54,10 +56,27 @@ public abstract class ServletUtils {
      *
      * @param request 请求
      * @param connection 连接
+     * @author c332030
+     */
+    public static void setHeaders(
+        @Nonnull HttpServletRequest request,
+        @Nonnull URLConnection connection
+    ) {
+        request.getHeaderNames().asIterator().forEachRemaining((String headerName) ->
+            setHeader(request, connection, headerName));
+    }
+
+    /**
+     * <p>
+     * Description: 设置请求头
+     * </p>
+     *
+     * @param request 请求
+     * @param connection 连接
      * @param httpHeaders 请求头集合
      * @author c332030
      */
-    public static void setHeader(
+    public static void setHeaders(
         @Nonnull HttpServletRequest request,
         @Nonnull URLConnection connection,
         @Nonnull String... httpHeaders
@@ -67,7 +86,69 @@ public abstract class ServletUtils {
         }
 
         for(String str: httpHeaders) {
-            connection.setRequestProperty(str, request.getHeader(str));
+            setHeader(request, connection, str);
+        }
+    }
+
+    /**
+     * <p>
+     * Description: 设置请求头
+     * </p>
+     *
+     * @param request 请求
+     * @param connection 连接
+     * @param httpHeaderName 请求头名称
+     * @author c332030
+     */
+    public static void setHeader(
+        @Nonnull HttpServletRequest request,
+        @Nonnull URLConnection connection,
+        @Nonnull String httpHeaderName
+    ) {
+        setHeader(request, connection, httpHeaderName, null);
+    }
+
+    /**
+     * <p>
+     * Description: 设置请求头
+     * </p>
+     *
+     * @param request 请求
+     * @param connection 连接
+     * @param httpHeaderName 请求头名称
+     * @param defaultValue 默认值
+     * @author c332030
+     */
+    public static void setHeader(
+        @Nonnull HttpServletRequest request,
+        @Nonnull URLConnection connection,
+        @Nonnull String httpHeaderName,
+        String defaultValue
+    ) {
+        String value = request.getHeader(defaultValue);
+        if(StringUtils.isEmpty(value)) {
+            value = defaultValue;
+        }
+        connection.setRequestProperty(httpHeaderName, value);
+    }
+
+
+    /**
+     * <p>
+     * Description: 设置请求头
+     * </p>
+     *
+     * @param connection 连接
+     * @param response 响应
+     * @author c332030
+     */
+    public static void setHeaders(
+        @Nonnull URLConnection connection,
+        @Nonnull HttpServletResponse response
+    ) {
+        Map<String, List<String>> map = connection.getHeaderFields();
+        for(Map.Entry<String, List<String>> entry: map.entrySet()) {
+            response.setHeader(entry.getKey(), StringUtils.join(entry.getValue(), ';'));
         }
     }
 
@@ -81,7 +162,7 @@ public abstract class ServletUtils {
      * @param httpHeaders 请求头集合
      * @author c332030
      */
-    public static void setHeader(
+    public static void setHeaders(
         @Nonnull URLConnection connection,
         @Nonnull HttpServletResponse response,
         @Nonnull String... httpHeaders
@@ -93,5 +174,48 @@ public abstract class ServletUtils {
         for(String str: httpHeaders) {
             response.setHeader(str, connection.getHeaderField(str));
         }
+    }
+
+
+    /**
+     * <p>
+     * Description: 设置请求头
+     * </p>
+     *
+     * @param connection 连接
+     * @param response 响应
+     * @param httpHeaderName 请求头名称
+     * @author c332030
+     */
+    public static void setHeader(
+        @Nonnull URLConnection connection,
+        @Nonnull HttpServletResponse response,
+        @Nonnull String httpHeaderName
+    ) {
+        setHeader(connection, response, httpHeaderName, null);
+    }
+
+    /**
+     * <p>
+     * Description: 设置请求头
+     * </p>
+     *
+     * @param connection 连接
+     * @param response 响应
+     * @param httpHeaderName 请求头名称
+     * @param defaultValue 默认值
+     * @author c332030
+     */
+    public static void setHeader(
+        @Nonnull URLConnection connection,
+        @Nonnull HttpServletResponse response,
+        @Nonnull String httpHeaderName,
+        String defaultValue
+    ) {
+        String value = connection.getHeaderField(defaultValue);
+        if(StringUtils.isEmpty(value)) {
+            value = defaultValue;
+        }
+        response.setHeader(httpHeaderName, value);
     }
 }
